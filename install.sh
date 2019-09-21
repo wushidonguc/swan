97e6a5b38b264864cb8d73470cbd01b09e750d65
Wushi Dong <33078715+wushidonguc@users.noreply.github.com>
2019-09-21 11:40:39 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+echo "Swan (Self-consistent Wannier-function-based quantum transport solver)"
+echo "by Wushi Dong, 2019"
+echo 'Compiling from source ...'
+echo '\n'
+
 # SPECIFY WHERE TO INSTALL EXECUTABLES
-INSTALL_DIR=../bin
+mkdir -p bin
 
 cd src
+mkdir -p obj
 make -f makefile.self_consistent
 make -f makefile.transport
-mv self_consistent transport ${INSTALL_DIR}
+
+echo '\n'
+echo "Done!"
+echo '\n'
+
+mv self_consistent transport ../bin
 cd ..
